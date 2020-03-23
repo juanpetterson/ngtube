83a242dbd326b4ed95b7150d714bde49a0349ba8
juanpetterson <juan.petterson@gmail.com>
2020-03-23 00:12:58 -0300

    Add sidebar routing and actived route styles

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BrowseComponent } from './browse/browse.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: BrowseComponent, pathMatch: 'full' },
+  { path: 'trending', component: BrowseComponent, pathMatch: 'full' },
+  { path: 'subscriptions', component: BrowseComponent },
+  { path: 'library', component: BrowseComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
